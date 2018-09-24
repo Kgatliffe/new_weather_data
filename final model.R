afbc2a0dec83d23b4fa40ab9aa65eeaecd1d0e28
Kathleen Gatliffe <kathleen.e.gatliffe@ucdenver.edu>
2018-09-23 23:37:46 -0600

    Add files via upload
    Final models (all times) and variable select code

--- final model.R
+++ final model.R
@@ -1,8 +1,26 @@
 glm(formula = as.factor(Question.1) ~ temp_24h_Den + pressure_24h_Den + 
-      weather_main_24h_Den_Dummy + humidity_24h_Den + rain_3h_24h_Den, 
-    family = binomial(), data = dat.train24)
+      weather_main_24h_Den_Dummy + humidity_24h_Den, family = binomial(), 
+    data = dat.train24)
 
 glm(formula = as.factor(Question.1) ~ pressure_12h_Den + weather_main_12h_Den_Dummy + 
       temp_12h_Den + humidity_12h_Den + weather_main_24h_Den_Dummy + 
-      rain_3h_12h_Den + clouds_all_12h_Den + rain_1h_12h_Den, family = binomial(), 
-    data = dat.train12, control = list(maxit = 50))
+      clouds_all_12h_Den, family = binomial(), data = dat.train12, 
+    control = list(maxit = 50))
+
+glm(formula = as.factor(Question.1) ~ weather_main_6h_Den_Dummy + 
+      pressure_6h_Den + temp_6h_Den + humidity_6h_Den + clouds_all_6h_Den + 
+      weather_main_24h_Den_Dummy + weather_main_12h_Den_Dummy + 
+      wind_speed_6h_Den, family = binomial(), data = dat.train6, 
+    control = list(maxit = 100))
+
+glm(formula = as.factor(Question.1) ~ weather_main_3h_Den_Dummy + 
+      pressure_3h_Den + humidity_3h_Den + temp_3h_Den + clouds_all_3h_Den + 
+      weather_main_6h_Den_Dummy + weather_main_12h_Den_Dummy + 
+      wind_speed_3h_Den + weather_main_24h_Den_Dummy, family = binomial(), 
+    data = dat.train3, control = list(maxit = 100))
+
+glm(formula = as.factor(Question.1) ~ weather_main_1h_Den_Dummy + 
+      pressure_1h_Den + weather_main_6h_Den_Dummy + temp_1h_Den + 
+      humidity_1h_Den + clouds_all_1h_Den + weather_main_12h_Den_Dummy + 
+      wind_speed_1h_Den + weather_main_3h_Den_Dummy + weather_main_24h_Den_Dummy, 
+    family = binomial(), data = dat.train1, control = list(maxit = 100))
